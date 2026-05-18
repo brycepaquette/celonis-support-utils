e588af86b3b56a6b9e233637c7a641cbb93da2ed
b-paquette <b.paquette@celonis.com>
2026-05-18 15:49:00 -0500

    Added DayOfWeek enum

--- src/celonis_support_utils/enums.py
+++ src/celonis_support_utils/enums.py
@@ -1,6 +1,18 @@
 from enum import Enum
 
 
+class DayOfWeek(Enum):
+    """Enum for days of the week."""
+
+    MO = 0
+    TUE = 1
+    WED = 2
+    THU = 3
+    FRI = 4
+    SAT = 5
+    SUN = 6
+
+
 class Region(Enum):
     """Enum for regions."""
 
