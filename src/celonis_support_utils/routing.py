6fa4cc0ca4bc4ffa766da2d3df09c3dc97445735
b-paquette <b.paquette@celonis.com>
2026-05-18 18:24:48 -0500

    implemented class protocol and standardrouting

--- src/celonis_support_utils/routing.py
+++ src/celonis_support_utils/routing.py
@@ -1,2 +1,31 @@
-class RoutingStrategy:
-    pass
+from typing import Protocol
+
+from celonis_support_utils.enums import Region
+
+from .engineer import Engineer
+from .team import Team
+from .ticket import Ticket
+
+
+class RoutingStrategy(Protocol):
+    def route(self, ticket: Ticket, teams: list[Team]) -> Engineer | None:
+        """
+        Determines which engineer should be assigned to the given ticket based
+        on the routing strategy.
+        Returns the assigned Engineer or None if no suitable engineer is found.
+        """
+        ...
+
+
+class StandardRouting:
+    def route(self, ticket: Ticket, teams: list[Team]) -> Engineer | None:
+        eligible_teams = [
+            team for team in teams if ticket.restriction in (Region.GLOBAL, team.region)
+        ]
+        for team in eligible_teams:
+            for engineer in team.engineers:
+                # TODO: Implement shift assignment logic here.
+                assigned_shift = None
+                if engineer.is_on_shift(assigned_shift):
+                    return engineer
+        return None
