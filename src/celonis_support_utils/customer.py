251b30c5740430c3c878375920e980709d8605cd
b-paquette <b.paquette@celonis.com>
2026-05-18 16:02:19 -0500

    implemented customer class

--- src/celonis_support_utils/customer.py
+++ src/celonis_support_utils/customer.py
@@ -1,2 +1,15 @@
+from enum import Enum
+
+
+class ServiceLevel(Enum):
+    STANDARD = "Standard"
+    PREMIUM = "Premium"
+    PREMIER = "Premier"
+    PREMIER_PLUS = "Premier Plus"
+    MAXSUCCESS = "MaxSuccess"
+
+
 class Customer:
-    pass
+    def __init__(self, company_name: str, service_level: ServiceLevel):
+        self.company_name = company_name
+        self.service_level = service_level
