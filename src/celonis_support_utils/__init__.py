941783797a279f395bdba22434efe2615a160895
b-paquette <b.paquette@celonis.com>
2026-05-18 18:31:36 -0500

    updated init.py to expose classes

--- src/celonis_support_utils/__init__.py
+++ src/celonis_support_utils/__init__.py
@@ -1,2 +1,23 @@
-def hello() -> str:
-    return "Hello from celonis-support-utils!"
+from .customer import Customer, ServiceLevel
+from .engineer import Engineer
+from .enums import DayOfWeek, Region
+from .queue import Queue
+from .routing import EscalationRouting, RoutingStrategy, StandardRouting
+from .shift import Shift
+from .team import Team
+from .ticket import Ticket
+
+__all__ = [
+    "Customer",
+    "ServiceLevel",
+    "Engineer",
+    "DayOfWeek",
+    "Region",
+    "Queue",
+    "EscalationRouting",
+    "RoutingStrategy",
+    "StandardRouting",
+    "Shift",
+    "Team",
+    "Ticket",
+]
