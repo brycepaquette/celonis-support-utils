94c506a7ba5746d9688add144ef58140151c1646
b-paquette <b.paquette@celonis.com>
2026-05-18 18:24:25 -0500

    added missing restriction property

--- src/celonis_support_utils/ticket.py
+++ src/celonis_support_utils/ticket.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from celonis_support_utils.enums import Region
+
 
 class IssueType(Enum):
     INCIDENT = "Incident"
@@ -17,6 +19,7 @@ class Ticket:
         product_area: str,
         title: str,
         description: str,
+        restriction: str,
     ):
         self.ticket_id = ticket_id
         self.issue_type = self._parse_issue_type(issue_type)
@@ -25,6 +28,7 @@ class Ticket:
         self.product_area = product_area
         self.title = title
         self.description = description
+        self.restriction = self._parse_restriction(restriction)
 
     @staticmethod
     def _parse_issue_type(value: str) -> IssueType:
@@ -34,4 +38,14 @@ class Ticket:
             valid_types = [t.name for t in IssueType]
             raise ValueError(
                 f"Invalid issue type: {exc}. Must be one of {valid_types}"
+            ) from exc
+
+    @staticmethod
+    def _parse_restriction(value: str) -> Region:
+        try:
+            return Region[value.strip().upper()]
+        except KeyError as exc:
+            valid_regions = [r.name for r in Region]
+            raise ValueError(
+                f"Invalid restriction: {exc}. Must be one of {valid_regions}"
             ) from exc
