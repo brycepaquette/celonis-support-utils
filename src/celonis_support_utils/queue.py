b4eb690cab8ce9bee6e93938135cd34f80618f92
b-paquette <b.paquette@celonis.com>
2026-05-18 17:54:19 -0500

    implemented queue

--- src/celonis_support_utils/queue.py
+++ src/celonis_support_utils/queue.py
@@ -1,2 +1,15 @@
+from celonis_support_utils.ticket import Ticket
+
+
 class Queue:
-    pass
+    def __init__(self, name: str):
+        self.name = name
+        self._queue: list[Ticket] = []
+
+    def add(self, ticket: Ticket) -> None:
+        self._queue.append(ticket)
+
+    def next(self) -> Ticket | None:
+        if not self._queue:
+            return None
+        return self._queue.pop(0)
