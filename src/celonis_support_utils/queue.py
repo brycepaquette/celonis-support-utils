f2304ca1cb866fcc6240298d9948b7766980f669
b-paquette <b.paquette@celonis.com>
2026-05-23 17:20:21 -0500

    docstrings

--- src/celonis_support_utils/queue.py
+++ src/celonis_support_utils/queue.py
@@ -1,4 +1,6 @@
 class Queue[T]:
+    """FIFO queue for ordered item processing."""
+
     def __init__(self, name: str):
         if not name:
             raise ValueError("Queue name cannot be empty.")
@@ -6,9 +8,11 @@ class Queue[T]:
         self._queue: list[T] = []
 
     def add(self, item: T) -> None:
+        """Add an item to the end of the queue."""
         self._queue.append(item)
 
     def next(self) -> T | None:
+        """Remove and return the next item, or None if empty."""
         if not self._queue:
             return None
         return self._queue.pop(0)
