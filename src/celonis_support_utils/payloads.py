82fb1e94795b7caea1e67c479cac2d21917ac00f
b-paquette <b.paquette@celonis.com>
2026-05-22 21:25:55 -0500

    added Literals to SalesforceTicketPayload

--- src/celonis_support_utils/payloads.py
+++ src/celonis_support_utils/payloads.py
@@ -1,13 +1,15 @@
-from typing import TypedDict
+from typing import Literal, TypedDict
 
 
 class SalesforceTicketPayload(TypedDict):
     ticket_id: str
-    issue_type: str
-    severity: str
+    issue_type: Literal["Incident", "Question", "Service Request"]
+    severity: Literal["", "SEV1", "SEV2", "SEV3", "SEV4"]
     service: str
     product_area: str
     title: str
     description: str
-    restriction: str
-    service_level: str
+    restriction: Literal["Global", "EU", "US", "APAC"]
+    service_level: Literal[
+        "Standard", "Premium", "Premier", "Premier Plus", "Max Success"
+    ]
