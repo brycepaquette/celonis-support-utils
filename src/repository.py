e230ed35d17a40fea1ecc18fd985a78d5f7555d3
b-paquette <b.paquette@celonis.com>
2026-05-23 13:04:04 -0500

    update repository.py

--- src/repository.py
+++ src/repository.py
@@ -9,3 +9,15 @@ class TicketRepository(Protocol):
     def get_by_id(self, ticket_id: str) -> Ticket | None: ...
 
     def list_open(self) -> list[Ticket]: ...
+
+
+class SalesforceTicketRepository:
+    # TODO: Implement SalesforceTicketRepository using Salesforce API
+    def save(self, ticket: Ticket) -> None:
+        raise NotImplementedError
+
+    def get_by_id(self, ticket_id: str) -> Ticket | None:
+        raise NotImplementedError
+
+    def list_open(self) -> list[Ticket]:
+        raise NotImplementedError
